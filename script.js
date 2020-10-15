ea7eaaadc9a633782fd9ce96c40cba27351fb167
Glitch (hello-webpage) <none>
2020-10-15 22:34:54 +0000

    😇🍕 Checkpoint ./script.js:23853715/293 ./style.css:23853715/241 ./index.html:23853715/267

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
-
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+function revealMe() {
+    document.getElementById("hiddenReveal").style.display = 'block';
+}
+//find the secret message on the website
