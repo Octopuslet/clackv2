b56eaaefb43ed612f1aea5b141d961a81af0a5e0
Glitch (hello-webpage) <none>
2020-11-08 23:40:30 +0000

    🐢🐙 Checkpoint ./style.css:23853715/4367 ./script.js:23853715/224 ./index.html:23853715/213

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function revealMe() {
-    document.getElementById("hiddenReveal").style.display = 'block';
+    alert()
 }
 
 //find the secret message on the website
+//(for function revealMe when i want to change what it does) document.getElementById("hiddenReveal").style.display = 'block';
