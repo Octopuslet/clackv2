1ef018da42c5b31f28fca53daf37c37d21eceadc
Glitch (hello-webpage) <none>
2020-12-07 12:20:25 +0000

    🌉🐦 Checkpoint ./index.html:23853715/258 ./script.js:23853715/3067 ./style.css:23853715/9087

--- script.js
+++ script.js
@@ -1,59 +1,15 @@
 function revealMe() {
-    alert()
+  alert();
 }
 
 //find the secret message on the website
 //(for function revealMe when i want to change what it does) document.getElementById("hiddenReveal").style.display = 'block';
 
-
-
- //function self_Checkout () {
-
-
-  /* var prices = [x, y, z,];
-  var x = prompt('Enter value');
-  var quantity_x = prompt('Enter value for quantity of item 1');
-  return x * quantity_x;}
-{ if 
-  var y = prompt('Enter value');
-  var quantity_y = prompt('Enter value for quantity of item 2');
-  return y * quantity_y;
-{ if
-  var z = prompt('Enter value');
-  var quantity_z = prompt('Enter value for quantity of item 3');
-  return z * quantity_y;
- 
- <script>
-function getit(){
-    var result = document.getElementById('demo');
-        var allitems = 0;
-            var itemCount = prompt("how many items do you need?");
-                var items = {};
-for (i = 0; i < itemCount; i++) {
-items[i] = {
-
-    name : prompt("Product Name"), 
-    price : prompt("Product Price"), 
-    qty : prompt("Product qty")
-
-    }
+let addToCart = document.getElementById("add-to-cart");
+let increment1 = document.getElementById("increment1");
+let decrement1 = document.getElementById("decrement1");
+let amount = document.getElementById("amount");
+let total = document.getElementById("total");
+function increment() {
+  amount.innerHTML = parseInt(amount.innerHTML)+1;
 }
-
-for (i = 0; i < itemCount; i++) {
-var subtotal = 0;
-var total = 0;
-subtotal = items[i].price * items[i].qty;
-total = subtotal * 1.05;
-allitems = allitems + subtotal; 
-result.innerHTML += "Product: " + items[i].name + "<br/>";
-result.innerHTML += "Total Qty: " +items[i].qty + "<br/>";
-result.innerHTML += "Sub total: " + subtotal + "<br/>";
-result.innerHTML += "Sub total: " + total + "<br/>";
-if(i == (itemCount - 1)){result.innerHTML += "Sub total for all items: " + allitems + "<br/>";}
-}
-    }
-</script>
-    
-
-<button onclick="getit()">Shop</button>
-<p id="result">Creating a JavaScript Object.</p> */
