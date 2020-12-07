ec2f7604a18ba11f25d6f02aa826c5cc23dbc572
Glitch (hello-webpage) <none>
2020-12-07 11:56:36 +0000

    🏊🏼🏚 Checkpoint ./script.js:23853715/1886 ./index.html:23853715/2968

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ function revealMe() {
 
 //find the secret message on the website
 //(for function revealMe when i want to change what it does) document.getElementById("hiddenReveal").style.display = 'block';
+
+
+
+ function self_Checkout () {
+
+
+  var prices = [x, y, z,];
+  var x = prompt('Enter value');
+  var quantity_x = prompt('Enter value for quantity of item 1');
+  return x * quantity_x;}
+{ if 
+  const y = prompt('Enter value');
+  var quantity_y = prompt('Enter value for quantity of item 2');
+  return y * quantity_y;
+{ if
+  var z = prompt('Enter value');
+  var quantity_z = prompt('Enter value for quantity of item 3');
+  return z * quantity_y;
