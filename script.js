81798b2f8ad537e48343c4e772153359846d0d36
Glitch (hello-webpage) <none>
2020-12-07 12:54:54 +0000

    👡😼 Checkpoint ./index.html:23853715/98 ./script.js:23853715/289

--- script.js
+++ script.js
@@ -5,11 +5,16 @@ function revealMe() {
 //find the secret message on the website
 //(for function revealMe when i want to change what it does) document.getElementById("hiddenReveal").style.display = 'block';
 
-let addToCart = document.getElementById("add-to-cart");
-let increment1 = document.getElementById("increment1");
-let decrement1 = document.getElementById("decrement1");
-let amount = document.getElementById("amount");
-let total = document.getElementById("total");
+let addToCart = document.querySelector("#add-to-cart");
+let increment1 = document.querySelector("#increment1");
+let decrement1 = document.querySelector("#decrement1");
+let amount = document.querySelector("#amount");
+let total = document.querySelector("#total");
 function increment() {
   amount.innerHTML = parseInt(amount.innerHTML)+1;
+  
+}
+function decrement() {
+  amount.innerHTML = parseInt(amount.innerHTML)-1;
+  
 }
