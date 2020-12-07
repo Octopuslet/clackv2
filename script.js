f7640f258800f76a983bce24053c5c3d19a3df93
Glitch (hello-webpage) <none>
2020-12-07 12:08:38 +0000

    🏉🍈 Checkpoint ./index.html:23853715/22017 ./script.js:23853715/3820 ./style.css:23853715/65

--- script.js
+++ script.js
@@ -7,18 +7,53 @@ function revealMe() {
 
 
 
- function self_Checkout () {
+ //function self_Checkout () {
 
 
-  var prices = [x, y, z,];
+  /* var prices = [x, y, z,];
   var x = prompt('Enter value');
   var quantity_x = prompt('Enter value for quantity of item 1');
   return x * quantity_x;}
 { if 
-  const y = prompt('Enter value');
+  var y = prompt('Enter value');
   var quantity_y = prompt('Enter value for quantity of item 2');
   return y * quantity_y;
 { if
   var z = prompt('Enter value');
   var quantity_z = prompt('Enter value for quantity of item 3');
   return z * quantity_y;
+ 
+ <script>
+function getit(){
+    var result = document.getElementById('demo');
+        var allitems = 0;
+            var itemCount = prompt("how many items do you need?");
+                var items = {};
+for (i = 0; i < itemCount; i++) {
+items[i] = {
+
+    name : prompt("Product Name"), 
+    price : prompt("Product Price"), 
+    qty : prompt("Product qty")
+
+    }
+}
+
+for (i = 0; i < itemCount; i++) {
+var subtotal = 0;
+var total = 0;
+subtotal = items[i].price * items[i].qty;
+total = subtotal * 1.05;
+allitems = allitems + subtotal; 
+result.innerHTML += "Product: " + items[i].name + "<br/>";
+result.innerHTML += "Total Qty: " +items[i].qty + "<br/>";
+result.innerHTML += "Sub total: " + subtotal + "<br/>";
+result.innerHTML += "Sub total: " + total + "<br/>";
+if(i == (itemCount - 1)){result.innerHTML += "Sub total for all items: " + allitems + "<br/>";}
+}
+    }
+</script>
+    
+
+<button onclick="getit()">Shop</button>
+<p id="result">Creating a JavaScript Object.</p> */
