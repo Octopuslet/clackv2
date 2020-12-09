65f1195fecdba4d79b6b4ccbbdb7a231f94d7774
Glitch (hello-webpage) <none>
2020-12-09 12:20:44 +0000

    🏀📅 Checkpoint ./index.html:23853715/10 ./script.js:23853715/757 ./style.css:23853715/2

--- script.js
+++ script.js
@@ -26,3 +26,27 @@ function addToCart2() {
   let total2 = parseInt(total.innerHTML.substring(7));
   total.innerHTML = 'total: ' + (total2+amount2*10.99).toFixed(2);
 }
+
+
+
+let addToCart3 = document.querySelector("#add-to-cart3");
+let increment2 = document.querySelector("#increment2");
+let decrement2 = document.querySelector("#decrement2");
+let amount9 = document.querySelector("#amount9");
+let totals = document.querySelector("#totals");
+function increments() {
+  let amount5 = parseInt(amount9.innerHTML)+1;
+  amount9.innerHTML = amount5.toFixed(0); 
+  
+  
+}
+function decrements() {
+ let amount5 = parseInt(amount9.innerHTML)-1;
+  amount9.innerHTML = amount5.toFixed(0);
+}
+
+function addToCarts() {
+  let amount9 = parseInt(amount9.innerHTML);
+  let totals = parseInt(totals.innerHTML.substring(7));
+  totals.innerHTML = 'total: ' + (totals+amount9*10.99).toFixed(2);
+}
