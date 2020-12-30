5321acd37786925c621ac35acb2f74407ff87a07
Glitch (hello-webpage) <none>
2020-12-30 18:31:34 +0000

    🎲🙈 Checkpoint ./script.js:23853715/1530 ./index.html:23853715/622 ./style.css:23853715/90

--- script.js
+++ script.js
@@ -42,3 +42,36 @@ function changeTotal(id) {
     console.log(total[id].innerHTML)
     total[id].innerHTML = 'total: ' + (_total+_amount*10.99).toFixed(2)+" €";
 }
+
+
+
+/*
+var bgDiv = $('#page1');
+var bgImg = $('#page1 > .background');
+
+var backgrounds = new Array(
+    'https://cdn.glitch.com/bc653cfc-ef90-45fa-b3ce-621a42c7ad62%2Fcomputer-keyboard-electronics-modern.jpg?v=1609114183583'
+  , 'https://cdn.glitch.com/bc653cfc-ef90-45fa-b3ce-621a42c7ad62%2Fphoto-1555066931-4365d14bab8c.webp?v=1609114185091'
+  , 'https://cdn.glitch.com/bc653cfc-ef90-45fa-b3ce-621a42c7ad62%2Fwallpapersden.com_programming-coding-language_1920x1080.jpg?v=1609114189007'
+  , 'https://cdn.glitch.com/bc653cfc-ef90-45fa-b3ce-621a42c7ad62%2F407978-programming-PHP-programming_language-syntax_highlighting-minified-knowledge-coding-code-HTML-computer-pixels-Computer_screen-logic-748x499.jpg?v=1609177511678'
+  , 'https://cdn.glitch.com/bc653cfc-ef90-45fa-b3ce-621a42c7ad62%2Fpexels-photo-177598.jpeg?v=1609177631960'
+  , 'https://cdn.glitch.com/bc653cfc-ef90-45fa-b3ce-621a42c7ad62%2Fistockphoto-1047259374-612x612.jpg?v=1609333792807'
+  , 'https://cdn.glitch.com/bc653cfc-ef90-45fa-b3ce-621a42c7ad62%2Finline_image_preview.jpg.webp?v=1609333794293'
+);
+
+function commandsOnClick(page) {
+  window.scrollTo(0, window.innerHeight * page);
+}
+var current = 0;
+
+function nextBackground() {
+    current++;
+    current = current % backgrounds.length; 
+  
+    bgDiv.css('background-image', 'url('+backgrounds[current]+')');
+    bgImg.attr('src', backgrounds[current]);
+}
+setInterval(nextBackground, 1500);
+
+bgDiv.css('background-image', 'url('+backgrounds[0]+')');
+bgImg.attr('src', backgrounds[0]); */
