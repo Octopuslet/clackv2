41ce13a4b70c9235d924728abde3f1e1d1d57de6
Glitch (hello-webpage) <none>
2020-12-31 14:24:03 +0000

    💿🛰 Checkpoint ./script.js:23853715/1124 ./index.html:23853715/184 ./style.css:23853715/32

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
-let topButtonTest = $('#page2 > .top-button-test');
-
 function commandsOnClick(page) {
   window.scrollTo(0, window.innerHeight * page);
 }
 
 
-function letThereBeLight() {
-  
-  
-  
-  
-  topButtonTest.css('visibility', 'visible');
-}
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("top-button").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "top-button") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
 /*let price = [
   10.99
 ]
