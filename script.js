864f89ada29dc3c01b2b800080a5504fcebea25b
Glitch (hello-webpage) <none>
2021-01-03 00:50:14 +0000

    👾🏮 Checkpoint ./index.html:23853715/379 ./style.css:23853715/713 ./script.js:23853715/1062

--- script.js
+++ script.js
@@ -35,14 +35,9 @@ function openList(evt, listDown) {
   evt.currentTarget.className += " active";
 }
 
-window.onscroll = function() {myFunction()};
 
-function myFunction() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.width = scrolled + "%";
-}
+
+
 /*let price = [
   10.99
 ]
